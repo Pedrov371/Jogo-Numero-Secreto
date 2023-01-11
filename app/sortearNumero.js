af7cae1dc83c0e6f634c0dcd5d7ce674ea0f47a0
Pedrov371 <pedroviniciusls895@gmail.com>
2023-01-11 19:19:24 -0300

    criando sistema para sortear números

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,2 +1,15 @@
-console.log("iniciando Gerenciador")
-console.log("conectando")
+const menorValor = 0;
+const maiorValor = 100;
+const numeroSecreto = gerarNumeroAleatorio();
+
+function gerarNumeroAleatorio () {
+    return parseInt(Math.random() * ( (maiorValor+1) - menorValor ) + menorValor)
+}
+
+console.log('numero secreto', numeroSecreto);
+
+const elementoMenorValor = document.getElementById('menor_valor');
+elementoMenorValor.innerHTML = menorValor
+
+const elementoMaiorValor = document.getElementById('maior_valor');
+elementoMaiorValor.innerHTML = maiorValor
